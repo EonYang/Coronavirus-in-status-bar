5446186e46f840e62372c2ddad72d4152d692a41
YangYang <yy2473@nyu.edu>
2020-02-23 16:33:37 -0500

    Initial Commit

---
+++ Cov_in_Status_Bar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cov_in_Status_Bar
+//
+//  Created by yangyang on 2/23/20.
+//  Copyright © 2020 yangyang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
